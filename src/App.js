564ec6253a05c3aa92bfb2b4eba68f36e6d0d48f
Zhiyuan He <zh2312@columbia.edu>
2021-05-06 22:06:55 -0400

    Prepare for responsive build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import './styles/app.css';
+import useWindowsSize from "./hooks/useWindowsSize";
+import "./styles/app.css";
 
 function App() {
-  return (
-    <div className='background'>
-        <div className='textbox'>
-          <h1 className='myname'>Michael He</h1>
-        </div>
-        <div className='textbox'>
-          <h2 className='mydesc'>Zealous, detail-oriented Software Engineer with a Graphic Design background and eye for design.</h2>
-        </div>
+  const { width: windowWidth } = useWindowsSize();
+  if (!windowWidth) return null;
 
-        <div style={{ height: 2000, width: "100%", backgroundColor: "#1c1c1c", position:"absolute", top:600, border:'5px solid white' }}></div>
-    </div>
-  )
+  return (
+    <>
+      <div
+        className="animated-background"
+        style={{ height: (windowWidth * 447) / 1920 }}
+      ></div>
+      <div className="textbox">
+        <h1 className="myname">Michael He</h1>
+        <h2 className="mydesc">
+          Zealous, detail-oriented Software Engineer with a Graphic Design
+          background and eye for design.
+        </h2>
+      </div>
+      <div
+        style={{
+          height: 2000,
+          width: "100%",
+          backgroundColor: "#1c1c1c",
+        }}
+      >
+        <div
+          className="container"
+        ></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
