99310594784fcc6f843a5f9752f290684bcf4940
Zhiyuan He <zh2312@columbia.edu>
2021-05-12 23:33:32 -0400

    Add a rotating effect for project showcase

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
   if (!windowWidth) return null;
   const handleClick = (tab) => (e) => {
     setActiveTab(tab);
-    sliderRef.current.style.left =
-      e.target.offsetLeft+ "px";
+    sliderRef.current.style.left = e.target.offsetLeft + "px";
   };
 
   return (
@@ -20,7 +19,9 @@ function App() {
         style={{ height: (windowWidth * 447) / 1920 }}
       ></div>
       <div className="title__textbox">
-        <h1 className="heading-primary--main"><span>I'm </span>Michael He</h1>
+        <h1 className="heading-primary--main">
+          <span>I'm </span>Michael He
+        </h1>
         <h2 className="heading-primary--sub">
           A zealous, detail-oriented Software Engineer with a Graphic Design
           background and eye for design.
@@ -35,18 +36,46 @@ function App() {
       >
         <div className="container">
           <div className="tabs">
-            <button onClick={handleClick(0)} className={`${activeTab === 0 && 'active-tab'}`}>Introduction</button>
-            <button onClick={handleClick(1)} className={`${activeTab === 1 && 'active-tab'}`}>Projects</button>
-            <button onClick={handleClick(2)} className={`${activeTab === 2 && 'active-tab'}`}>Skills</button>
-            <button onClick={handleClick(3)} className={`${activeTab === 3 && 'active-tab'}`}>Connect</button>
+            <button
+              onClick={handleClick(0)}
+              className={`${activeTab === 0 && "active-tab"}`}
+            >
+              Introduction
+            </button>
+            <button
+              onClick={handleClick(1)}
+              className={`${activeTab === 1 && "active-tab"}`}
+            >
+              Projects
+            </button>
+            <button
+              onClick={handleClick(2)}
+              className={`${activeTab === 2 && "active-tab"}`}
+            >
+              Skills
+            </button>
+            <button
+              onClick={handleClick(3)}
+              className={`${activeTab === 3 && "active-tab"}`}
+            >
+              Connect
+            </button>
           </div>
-          <div
-            className="slider"
-            ref={sliderRef}
-          ></div>
-          <div>
-            <div className="card">
-              TEXT
+          <div className="slider" ref={sliderRef}></div>
+          <div style={{ display: "flex" }}>
+            <div style={{ width: 500, marginRight: 24 }}>
+              <div className="card">
+                <div className="card__side card__side--front">
+                  <div class="card__picture card__picture--1">&nbsp;</div>
+                  Project 1
+                </div>
+                <div className="card__side card__side--back">Tech Stack</div>
+              </div>
+            </div>
+            <div style={{ width: 500 }}>
+              <div className="card">
+                <div className="card__side">TEXT</div>
+              </div>
             </div>
           </div>
         </div>
