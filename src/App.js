f4830e14754487070e7aa46f3d723d07c28595e1
Zhiyuan He <zh2312@columbia.edu>
2021-05-19 16:37:27 -0400

    Finish beta

--- src/App.js
+++ src/App.js
@@ -185,7 +185,7 @@ function App() {
                       </span>
                     </h4>
                     <div className="card__details">
-                      A project to get hands-on experience of React hooks. Also
+                      A MERN project to get hands-on experience of React hooks. Also
                       a part of experiment of providing small business solution.
                     </div>
                   </div>
@@ -267,10 +267,219 @@ function App() {
                   </div>
                 </div>
               </div>
+              <div style={{ width: 400, margin: "30px 56px" }}>
+                <div className="card">
+                  <div className="card__side card__side--front">
+                    <div class="card__picture card__picture--5">&nbsp;</div>
+                    <h4 class="card__heading">
+                      <span class="card__heading-span card__heading-span--5">
+                        My Portfolio
+                      </span>
+                    </h4>
+                    <div className="card__details">
+                      A project to to renew my portfolio site and graphic design skill. 
+                      Also to try SCSS with React
+                    </div>
+                  </div>
+                  <div className="card__side card__side--back">
+                    <div className="card__back-tech">
+                      <div className="card__cta">
+                        <ul>
+                          <li>React</li>
+                          <li>SCSS</li>
+                          <li>Photoshop</li>
+                        </ul>
+                        <a
+                          href="https://zbmike.dev/"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="btn btn--white"
+                        >
+                          Live
+                        </a>
+                        <br />
+                        <br />
+                        <a
+                          href="https://github.com/zbmike/porfolio"
+                          target="_blank"
+                          rel="noreferrer"
+                          className="btn btn--white"
+                        >
+                          Code
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          )}
+          {activeTab === 2 && (
+            <>
+              <br />
+              <ul className="skills">
+                <li className="style1">
+                  <span
+                    className="devicon-ruby-plain-wordmark"
+                    style={{ fontSize: 120, margin: (1000 - 600) / 10 }}
+                    title="Ruby"
+                  ></span>
+                </li>
+                <li className="style2">
+                  <span
+                    className="devicon-javascript-plain"
+                    style={{ fontSize: 120, margin: (1000 - 600) / 10 }}
+                    title="JavaScript"
+                  ></span>
+                </li>
+                <li className="style3">
+                  <span
+                    className="devicon-rails-plain-wordmark"
+                    style={{ fontSize: 120, margin: (1000 - 600) / 10 }}
+                    title="Ruby on Rails"
+                  ></span>
+                </li>
+                <li className="style4">
+                  <span
+                    className="devicon-nodejs-plain"
+                    style={{ fontSize: 120, margin: (1000 - 600) / 10 }}
+                    title="Node.js"
+                  ></span>
+                </li>
+                <li className="style5">
+                  <span
+                    className="devicon-react-original"
+                    style={{ fontSize: 120, margin: (1000 - 600) / 10 }}
+                    title="React"
+                  ></span>
+                </li>
+              </ul>
+              <ul className="skills">
+                <li className="style2">
+                  <span
+                    className="devicon-express-original"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="Express.js"
+                  ></span>
+                </li>
+                <li className="style3">
+                  <span
+                    className="devicon-mongodb-plain"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="MongoDB"
+                  ></span>
+                </li>
+                <li className="style4">
+                  <span
+                    className="devicon-html5-plain-wordmark"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="HTML5"
+                  ></span>
+                </li>
+                <li className="style5">
+                  <span
+                    className="devicon-css3-plain-wordmark"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="CSS3"
+                  ></span>
+                </li>
+                <li className="style1">
+                  <span
+                    className="devicon-python-plain-wordmark"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="Python"
+                  ></span>
+                </li>
+                <li className="style2">
+                  <span
+                    className="devicon-angularjs-plain"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="C"
+                  ></span>
+                </li>
+                <li className="style3">
+                  <span
+                    className="devicon-photoshop-plain"
+                    style={{ fontSize: 80, margin: (1000 - 560) / 14 }}
+                    title="Photoshop"
+                  ></span>
+                </li>
+              </ul>
+              <h1 className="skill-text">And More... </h1>
+            </>
+          )}
+          {activeTab === 3 && (
+            <section className="main special">
+              <header className="major">
+                <h1>Connect</h1>
+                <p>
+                  Thanks for taking the time to go through my work! <br />
+                  To learn more about me, check out my{" "}
+                  <a href="assets/Zhiyuan_He_Resume.pdf">
+                    <strong>resume</strong>
+                  </a>{" "}
+                  and/or connect with me on{" "}
+                  <a href="https://www.linkedin.com/in/zhiyuan-he-mike/">
+                    <strong>Linkedin</strong>
+                  </a>
+                  ,{" "}
+                  <a href="https://angel.co/zhiyuan-he-2/">
+                    <strong>Angel List</strong>
+                  </a>{" "}
+                  and{" "}
+                  <a href="https://github.com/zbmike/">
+                    <strong>GitHub</strong>
+                  </a>.<br/>
+                   You can also email me at{" "}
+                  <a href="mailto:zbmanhe@gmail.com">
+                    <strong>zbmanhe@gmail.com</strong>
+                  </a>
+                  .
+                </p>
+              </header>
+            </section>
           )}
         </div>
       </div>
+      <footer id="footer">
+        <section>
+          <h2>Michael He on the way to his dream.</h2>
+        </section>
+        <div class="full-width">
+          <ul class="icons">
+            <li>
+              <a
+                href="https://angel.co/zhiyuan-he-2/"
+                class="icon brands fa-angellist alt"
+              >
+                <span class="label">Agnel List</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/zbmike/"
+                class="icon brands fa-github alt"
+              >
+                <span class="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/zhiyuan-he-mike/"
+                class="icon brands fa-linkedin alt"
+              >
+                <span class="label">Linkedin</span>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:zbmanhe@gmail.com" class="icon fa-envelope alt">
+                <span class="label">Email</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p class="copyright">&copy; Mike He.</p>
+      </footer>
     </>
   );
 }
